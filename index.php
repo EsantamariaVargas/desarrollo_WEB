aab1fb0a1d08c8b2994e39f708bc60ccebb223c1
EsantamariaVargas <95193030+EsantamariaVargas@users.noreply.github.com>
2021-11-29 15:03:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ejercicio 4</title>
+    <link href="css1/media_query.css" rel="stylesheet" type="text/css"/>
+    <link href="css1/diseno.css" rel="stylesheet" type="text/css"/>
+    <link href="css1/animate.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link href="css1/owl.carousel.css" rel="stylesheet" type="text/css"/>
+    <link href="css1/owl.theme.default.css" rel="stylesheet" type="text/css"/>
+    <link href="css1/style_1.css" rel="stylesheet" type="text/css"/>
+    <script src="js1/modernizr-3.5.0.min.js"></script>
+</head>
+<body>
+<div class="container-fluid fh5co_header_bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 fh5co_mediya_center"><a href="#" class="color_fff fh5co_mediya_setting">
+				
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-faded fh5co_padd_mediya padding_786">
+    <div class="container padding_786">
+        <nav class="navbar navbar-toggleable-md navbar-light ">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuButton2" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">Menu<span class="sr-only">(current)</span></a>
+                        
+                    </li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="" >Salir</a>                        						
+					</li>	
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+<header>
+<h1>
+<center> 
+Ejercicio 4
+</center> 
+</h1>
+</header>
+<br>
+<br>
+<table WIDTH="100%" > 
+  <tr>
+    <td>
+		<center><a href="https://www.w3schools.com/tags/tag_form.asp"><img src="des.web.jpg" alt="" width="150" height="150"></a></center>
+	</td>
+    <td>
+		<p><h5>W3 Schools</h5></p><br>
+		<p><h7>Dando click en la imagen iras al sitio w3 school que se utilizó como material de apoyo para la creación de la página web</h7></p><br>
+	</td>
+	 <td WIDTH="30%">
+	
+			<fieldset>
+			<p><h7>Elliot Fu</h7></p>
+
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			
+			<img src="img_Icono.jpg" alt="" width="50" height="50"><br>
+			<p><h7>Friends of veronika</h7></p><br>
+			<p><h7>Elliot requested permission to view your contact detalis</h7></p><br>
+			  <center><button  onclick="location.href='https://www.esviernes.com/';" style="background-color:white; border-color:green; color:green">decline</button>
+			  <button  onclick="location.href='https://www.esviernes.com/';" style="background-color:white; border-color:red; color:red">decline</button></center>
+		</fieldset>
+	</td>
+   </tr>
+   
+   <tr>
+    <td>
+		<center><a href="https://www.w3schools.com/tags/tag_form.asp"><img src="des.web.jpg" alt="" width="150" height="150"></a></center>
+	</td>
+    <td>
+		<p><h5>W3 Schools</h5></p><br>
+		<p><h7>Dando click en la imagen iras al sitio w3 school que se utilizó como material de apoyo para la creación de la página web</h7></p><br>
+	</td>
+	 <td>
+	
+	</td>
+   </tr>
+   
+   <tr>
+    <td>
+		<center><a href="https://www.w3schools.com/tags/tag_form.asp"><img src="des.web.jpg" alt="" width="150" height="150"></a></center>
+	</td>
+    <td>
+		<p><h5>W3 Schools</h5></p><br>
+		<p><h7>Dando click en la imagen iras al sitio w3 school que se utilizó como material de apoyo para la creación de la página web</h7></p><br>
+	</td>
+	 <td>
+		<fieldset>
+			<p><h7>Elliot Fu</h7></p>
+
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			
+			<img src="img_Icono.jpg" alt="" width="50" height="50"><br>
+			<p><h7>Friends of veronika</h7></p><br>
+			<p><h7>Elliot requested permission to view your contact detalis</h7></p><br>
+			   <center><button  onclick="location.href='https://www.esviernes.com/';" style="background-color:white; border-color:green; color:green">decline</button>
+			  <button  onclick="location.href='https://www.esviernes.com/';" style="background-color:white; border-color:red; color:red">decline</button></center>
+		</fieldset>
+	</td>
+   </tr>
+</table>
+
+<div class="container-fluid fh5co_footer_right_reserved">
+    <div class="container">
+        <div class="row" align='center'>
+            <div class="col-12 col-md-12 py-4 Reserved"> <br> Diseñado por <a href="" title="Free HTML5 Bootstrap templates">Esteban Santamaria Vargas</a>. </div>
+        </div>
+    </div>
+</div>
+
+<div class="gototop js-top">
+    <a href="#" class="js-gotop"><i class="fa fa-arrow-up"></i></a>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+        integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+        crossorigin="anonymous"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Main -->
+<script src="js/main.js"></script>
+
+</body>
+</html>
